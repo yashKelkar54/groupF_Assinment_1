04ec43bbe4ebf948893566ff686388d4cfa6303d
Hardik Kumar <hardikkumar32@gmail.com>
2023-09-21 20:16:44 -0400

    added max function

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(num1, num2 int) int {
+	/* local variable declaration */
+	var result int
+
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
+
+func main() {
+	var a int = 100
+	var b int = 200
+	var ret int
+
+	/* calling a function to get max value */
+	ret = max(a, b)
+
+	fmt.Printf("Max value is : %d\n", ret)
 }
